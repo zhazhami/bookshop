babfe0a37550ec612eb9130acaa64a5c833d649d
zhazhami <541395961@qq.com>
2016-11-05 15:01:04 +0800

    database

---
+++ app/Entity/Pdt_images.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Entity;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pdt_images extends Model
+{
+    protected $table = 'pdt_images';
+    protected $primaryKey = 'id';
+}
